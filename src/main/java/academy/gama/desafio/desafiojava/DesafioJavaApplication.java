dd41b187b73200318ae3450d96b8c2e3b931fe2c
kellisonjk <kellisonjk@gmail.com>
2021-02-09 00:03:54 -0300

    Initial commit

---
+++ src/main/java/academy/gama/desafio/desafiojava/DesafioJavaApplication.java
@@ -0,0 +1,13 @@
+package academy.gama.desafio.desafiojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJavaApplication.class, args);
+	}
+
+}
