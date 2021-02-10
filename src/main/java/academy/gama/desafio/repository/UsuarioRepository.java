0b24d010deee13e53b05b2a8458583db2e06da19
Alessandra Canuto <ale.canutto@gmail.com>
2021-02-09 22:24:23 -0300

    add usuario

---
+++ src/main/java/academy/gama/desafio/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package academy.gama.desafio.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import academy.gama.desafio.model.Usuario;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+	
+	Optional<Usuario> findByLogin(String login);
+	
+}
