8d3aa4dab590f078f29f2f08929dba0c37063d73
Raul Castro <raulc27@gmail.com>
2021-02-10 22:42:50 -0300

    passando a trabalhar com JpaRepository

--- src/main/java/academy/gama/desafio/repository/PlanoContasRepository.java
+++ src/main/java/academy/gama/desafio/repository/PlanoContasRepository.java
@@ -1,13 +1,14 @@
 package academy.gama.desafio.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+import academy.gama.desafio.model.Conta;
 import academy.gama.desafio.model.PlanoContas;
+import academy.gama.desafio.model.Usuario;
 
-public interface PlanoContasRepository extends CrudRepository<PlanoContas, Long> {
+public interface PlanoContasRepository extends JpaRepository<PlanoContas, Long> {
 
-	
-	
 	
 }
