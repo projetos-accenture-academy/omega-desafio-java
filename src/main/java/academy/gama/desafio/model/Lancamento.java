415eb138741f05bcad3f8ef5f5587f69b94abebf
bconfessor <brian_confessor@poli.ufrj.br>
2021-02-11 02:04:30 -0300

    Criação De Lançamento
    Modelo básico de Lançamento com Getters e Setters de seus atributos (exceto ID, auto-incrementada pelo BD)

---
+++ src/main/java/academy/gama/desafio/model/Lancamento.java
@@ -0,0 +1,94 @@
+package academy.gama.desafio.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "lancamento")
+public class Lancamento {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	//TODO: Inserir FK PlanoConta
+
+	//Is null when it's a deposit
+	@JoinColumn (name = "id_conta_origem", nullable = true)
+	@ManyToOne
+	private Conta contaOrigem;
+	
+	//It's null when it's a withdrawal
+	@JoinColumn(name = "id_conta_destino", nullable = true)
+	@ManyToOne
+	private Conta contaDestino;
+	
+	@Column
+	private LocalDate data;
+	
+	@Column
+	private Double valor;
+	
+
+	@Column
+	private String descricao;
+	
+	
+	
+	public Lancamento()
+	{
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Conta getContaOrigem() {
+		return contaOrigem;
+	}
+
+	public void setContaOrigem(Conta contaOrigem) {
+		this.contaOrigem = contaOrigem;
+	}
+
+	public Conta getContaDestino() {
+		return contaDestino;
+	}
+
+	public void setContaDestino(Conta contaDestino) {
+		this.contaDestino = contaDestino;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
