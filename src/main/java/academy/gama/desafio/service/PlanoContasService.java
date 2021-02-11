af9a115f37069f8ed2366ca0b8017e75d0adc111
Raul Castro <raulc27@gmail.com>
2021-02-11 02:41:19 -0300

    ultimas alteracoes

--- src/main/java/academy/gama/desafio/service/PlanoContasService.java
+++ src/main/java/academy/gama/desafio/service/PlanoContasService.java
@@ -3,6 +3,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import academy.gama.desafio.model.PlanoContas;
 import academy.gama.desafio.model.Usuario;
+import academy.gama.desafio.model.Lancamento;
 import academy.gama.desafio.repository.PlanoContasRepository;
 import academy.gama.desafio.repository.UsuarioRepository;
 
